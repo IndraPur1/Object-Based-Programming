aeebb704033469d786c1edb4f270e8cd1a4244fe
Indra Purwanto <151821306+IndraPur1@users.noreply.github.com>
2025-03-09 20:50:44 +0700

    Add files via upload

---
+++ Pertemuan ke 3/Tugas Tambahan/Asertion/Tiket.java
@@ -0,0 +1,44 @@
+/* Nama File    : Tiket.java
+ * Deskripsi    : class Tiket, berisi method untuk memesan tiket dan memilih e-wallet
+ * Pembuat      : Indra Purwanto/24060123140150
+ * Tanggal      : 7 Maret 2025
+ */
+
+public class Tiket {
+   public static void pesanJumlahTiket(int jumlahTiket) {
+      // Asumsi jumlah tiket yang tersedia (boleh diganti)
+      int tiketTersedia = 100;
+
+      // Assertion untuk memastikan jumlah pemesanan lebih dari 0
+      assert(jumlahTiket > 0): "Jumlah pemesanan tidak boleh nol!";
+
+      // Assertion untuk memastikan jumlah pemesanan tidak melebihi tiket yang tersedia
+      assert(jumlahTiket <= tiketTersedia): "Jumlah pemesanan melebihi tiket yang tersedia!";
+      System.out.println("Pemesanan tiket sebanyak " + jumlahTiket + " berhasil.");
+   }
+
+   public static void isEWallet(int EWallet) {
+      // Memeriksa apakah index e-wallet yang dipilih valid
+      if (EWallet > 0 && EWallet <= 4) {
+         System.out.println("Pemesanan tiket selesai menggunakan e-wallet " + getEWallet(EWallet));
+      // Assertion untuk memastikan e-wallet yang dipilih valid
+      } else {
+         assert(EWallet > 0 && EWallet <= 4): "Pilihan e-wallet tidak valid!";
+      }
+   }
+
+   private static String getEWallet(int index) {
+      switch (index) {
+         case 1:
+            return "OVO";
+         case 2:
+            return "GoPay";
+         case 3:
+            return "DANA";
+         case 4:
+            return "LinkAja";
+         default:
+            return "";
+      }
+   }
+}
---
+++ Pertemuan ke 3/Tugas Tambahan/Exception/JumlahPesananMelebihiStokException.java
@@ -0,0 +1,12 @@
+/* Nama File    : JumlahPesananMelebihiStokException.java
+ * Deskripsi    : exception ketika pesanan lebih dari stok
+ * Pembuat      : Indra Purwanto/24060123140150
+ * Tanggal      : 7 Maret 2025
+ */
+
+public class JumlahPesananMelebihiStokException extends Exception {
+    // TO DO 2: Buat konstructor dengan parameter message dan panggil konstruktor parent dengan keyword super
+    public JumlahPesananMelebihiStokException(String message){
+        super(message);
+    }
+}
